8f999759bccbc03521184c4a40d331acdd9c7daf
Isha Sidhu <ishasimranxo@gmail.com>
2021-03-20 12:30:06 -0600

    initial commit

---
+++ public/js/client.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// const endpoint = '//localhost:3000/api/v0/gallery';     
+const endpoint = '';
+
+fetch(endpoint)
+
+  .then(function (response) {
+    if (!response.ok) {
+      throw new Error('Not 200 OK');
+    }
+    return response.json();
+  })
+
+  .then(function (gallery) {
+    let figure = document.querySelector('figure');
+    gallery.forEach(insert_img);
+    function insert_img(item, index) {
+      figure.innerHTML = figure.innerHTML + `<img src="${gallery[index].pathURL}" alt="${gallery[index].description}"></img><figcaption>${gallery[index].description}</figcaption>`;
+    };
+  })
+
+  .catch(function (error) {
+    console.log(error);
+  });
---
+++ server.js
@@ -0,0 +1,50 @@
+//  Load builtin modules
+const path = require('path')
+
+//  Load npm modules
+const express = require('express')
+const dotenv = require('dotenv').config()
+const cors = require('cors')
+
+//  Load custom modules
+const route_one = require('./routes/api/v0.js')
+const route_two = require('./routes/index.js')
+
+//  Assign the express() function
+const app = express()
+
+//  Set the view engine to 'ejs"
+app.set('view engine', 'ejs');
+
+//  must be invoked in order to pull data from Heroku 
+app.use(cors())
+
+//  Sets the global template variables
+app.use(function (req, res, next) {
+  res.locals = {
+    siteHeading: "CPNT262 - A4",
+    subHeading: "JSON Gallery",
+    copyright: "Isha Simran"
+  }
+  next()
+})
+
+//  Static page handler
+app.use(express.static(path.join(__dirname, 'public')))
+
+//  Route handlers
+app.use('/', route_one)
+app.use('/', route_two)
+
+//  Error handler
+app.use(function (req, res) {
+  res.status(404)
+  res.sendFile(__dirname + '/public/404.html')
+})
+
+const port = process.env.PORT || 3000
+
+//  Listening block
+app.listen(port, function () {
+  console.log(`Listening on port: ${port}`)
+})
